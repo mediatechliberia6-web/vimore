58215a25bdea0b898099f00e134e98c9ca83aad9
Amos B Kortu <amoskortub@gmail.com>
2026-03-16 01:09:22 +0000

    Yes

--- src/components/layout/sub-header.tsx
+++ src/components/layout/sub-header.tsx
@@ -9,7 +9,7 @@ import {
   Search, 
   Music2,
   Coins,
-  Gem,
+  Gem, 
   Star,
   User,
   Wallet,
@@ -22,6 +22,7 @@ import {
 } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
 import { cn } from "@/lib/utils";
 import { usePathname, useRouter } from "next/navigation";
 import { usePosts } from "@/context/PostContext";
